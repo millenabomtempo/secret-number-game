1c255eb69df7e8d768b21462e10be450d5cb521d
Millena Bomtempo <millena.bomtempo@gmail.com>
2023-01-02 11:58:26 -0300

    working with web speech api

---
+++ app/voiceRecognition.js
@@ -0,0 +1,24 @@
+const shotElement = document.getElementById('chute')
+
+window.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.leng = 'pt-Br'
+recognition.start()
+
+recognition.addEventListener('result', onSpeak)
+
+function onSpeak(e) {
+  const shot = e.results[0][0].transcript
+
+  showShot(shot)
+
+}
+
+function showShot(shot) {
+  shotElement.innerHTML = `
+    <div>Você disse:</div>
+    <span class="box">${shot}</span>
+  `
+}
